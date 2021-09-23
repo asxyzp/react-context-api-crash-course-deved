d977c58f890f3d2d8164a88ebfa3aeaa83dc53e5
asxyzp <asxyzp@gmail.com>
2021-09-24 02:17:47 +0530

    Entry point files

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ElephantList from './ElephantList';
+
+function App() {
+  return (
+    <div className="w-100 d-flex flex-wrap justify-content-center align-items-stretch">
+      <ElephantList/>
+    </div>
+  );
+}
+
+export default App;
