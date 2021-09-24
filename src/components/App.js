9e93fc0ea0046c61bef3dd9ee11366574cb41643
asxyzp <asxyzp@gmail.com>
2021-09-24 20:57:11 +0530

    + Passing state using Context API + Using useState to access Source

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import {ElephantList} from './ElephantList';
+import { Source } from "../data/Source";
+import React, { useState, createContext } from 'react';
+import ElephantList from './ElephantList';
+import SearchBar from './SearchBar';
 
-function App() {
-  return (
-    <div className="w-100 d-flex flex-wrap justify-content-center align-items-stretch">
-      <ElephantList/>
-    </div>
-  );
+export const ElephantContext = createContext();
+export const App = () => {
+
+    const [elephantList, setElephantList] = useState(Source);                                      //Setting the state containing the list of elephants
+    const [searchList, setSearchList] = useState(elephantList.map(elephant=> {return elephant;})); //Setting the state containing the list of searched elements
+    const contextVal = [[elephantList, setElephantList],[searchList, setSearchList]];              //Context value passed to the element
+    return (
+        <ElephantContext.Provider value={contextVal}>
+            <SearchBar />
+            <div className="w-100 d-flex flex-wrap justify-content-center align-items-stretch">
+                <ElephantList />
+            </div>
+        </ElephantContext.Provider>
+
+    );
 }
-
-export default App;
